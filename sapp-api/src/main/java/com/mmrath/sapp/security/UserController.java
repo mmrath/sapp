5bb7a3cf6cb93cad9ee1d68cfcc5631aeb92cbe9
murali <mmrath@gmail.com>
2015-01-07 22:43:23 +1100

    minor file movements

---
+++ sapp-api/src/main/java/com/mmrath/sapp/security/UserController.java
@@ -0,0 +1,12 @@
+package com.mmrath.sapp.security;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by murali on 7/01/15.
+ */
+@RestController
+public class UserController {
+
+
+}
