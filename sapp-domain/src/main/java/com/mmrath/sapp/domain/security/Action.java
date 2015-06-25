83840a57966e3f90e21f228610867185910a9f96
mmrath <mmrath@gmail.com>
2015-06-26 07:44:31 +1000

    Minor changes

---
+++ sapp-domain/src/main/java/com/mmrath/sapp/domain/security/Action.java
@@ -0,0 +1,12 @@
+package com.mmrath.sapp.domain.security;
+
+/**
+ * Represents action on a resource
+ */
+public enum Action {
+    READ,
+    CREATE,
+    UPDATE,
+    DELETE,
+    ALL
+}
---
+++ sapp-domain/src/main/java/com/mmrath/sapp/domain/security/Resource.java
@@ -0,0 +1,10 @@
+package com.mmrath.sapp.domain.security;
+
+/**
+ * Represents a resource that should be protected
+ */
+public enum Resource {
+    USER,
+    ROLE,
+    GROUP
+}
